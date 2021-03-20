a6f72302cd8bfb74e43e5940d00b1651dde19ffc
Raflazer <rafsibal03@gmail.com>
2021-03-21 01:14:32 +0800

    Finalize Main class

--- FacebookSignUp.java
+++ FacebookSignUp.java
@@ -17,6 +17,15 @@ public class FacebookSignUp {
      */
     public static void main(String[] args) {
     
+Function function = new Function();
+
+    System.out.println(function.signup());
+    
+    function.functionName();
+    function.functionPEC();
+    
+    System.out.println(function.welcome());
+    System.out.println( function.result() );
     
     }
     
